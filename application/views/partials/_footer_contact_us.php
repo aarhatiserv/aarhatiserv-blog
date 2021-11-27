831ce8eb28444c79a45845edc1a9b18aed99459a
mr-rakesh-ranjan <rakeshranjanhjp@gmail.com>
2021-11-27 13:41:10 +0530

    modifiction in footer

---
+++ application/views/partials/_footer_contact_us.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+
+<!--Partial: Footer Random Posts-->
+<div class="footer-widget f-widget-random">
+    <div class="col-sm-12">
+        <div class="row">
+            <h4 class="title"> <?php echo html_escape("contact us"); ?></h4>
+            <div class="title-line"></div>
+            <p>
+            <i class="bi bi-house-door-fill"></i> &nbsp;AC 97/1 prafulla kanan(east)<br>   &nbsp; &nbsp; &nbsp;&nbsp;kestopur kolkata-700 101
+            </p>
+            <p>
+                <i class="icon-envelope mr-3"></i>  &nbsp;Support@aarhatiserv.com
+            </p>
+            <p>
+                <i class="icon-phone mr-3"></i> &nbsp;+9179083 82268
+            </p>
+            
+        </div>
+    </div>
+</div>
